423c172896b883c1ecc4a1cd01c6586ec479281d
RsDev12 <digo.s.carvalho.2002@gmail.com>
2026-06-05 16:03:10 -0300

    Update MySQL_Xampp.sql

--- Projeto.GestãoCategoria/banco/MySQL_Xampp.sql
+++ Projeto.GestãoCategoria/banco/MySQL_Xampp.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `Categoria`
 --
-
+CREATE database GestaoCategoria;
+USE GestaoCategoria;
 DROP TABLE IF EXISTS `Categoria`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
